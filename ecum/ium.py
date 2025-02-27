63b04db5a2950a0740ad045ca11a2252ef88730c
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-26 23:13:38 -0500

    Se ajusta lectura de loas archivos parquet

--- ecum/ium.py
+++ ecum/ium.py
@@ -5,6 +5,7 @@ import nltk
 from nltk.tokenize import wordpunct_tokenize
 from nltk.corpus import stopwords
 import numpy as np
+import os
 
 # Cargar las variables globalmente una sola vez
 patrones = None
@@ -28,10 +29,25 @@ def load_patterns():
     ]
 
 def load_parametric_tables():
-    TPECUM = pd.read_parquet("data\\TPECUM.parquet")
+
+    # Obtener la ruta absoluta de la carpeta donde se encuentra este script (ium.py)
+    base_path = os.path.dirname(os.path.abspath(__file__))  
+
+    # Subir un nivel y acceder a la carpeta donde está el archivo
+    parent_path = os.path.abspath(os.path.join(base_path, "..", "data"))
+    # Construir la ruta completa del archivo
+    file_path = os.path.join(parent_path, "TPECUM.parquet")
+    TPECUM = pd.read_parquet(file_path)
     DICRE = {j: TPECUM[TPECUM['CLASL'] == j][['RE', 'CLASLR']].to_numpy() for j in TPECUM['CLASL'].unique()}
 
-    TPECUM_SEC = pd.read_parquet("data\\TPECUM_SEC.parquet")
+
+    # Obtener la ruta absoluta de la carpeta donde se encuentra este script (ium.py)
+    base_path = os.path.dirname(os.path.abspath(__file__))  
+    # Subir un nivel y acceder a la carpeta donde está el archivo
+    parent_path = os.path.abspath(os.path.join(base_path, "..", "data"))
+    # Construir la ruta completa del archivo
+    file_path = os.path.join(parent_path, "TPECUM_SEC.parquet")
+    TPECUM_SEC = pd.read_parquet(file_path)
     lpatrones = TPECUM_SEC[['PATRON', 'cod_err']].to_numpy()
     return DICRE, lpatrones
 
