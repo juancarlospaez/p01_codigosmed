238d05e8672d6be6a5939c4152a036cecbc7aae3
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-26 23:34:58 -0500

    Se ajusta el stup

--- setup.py
+++ setup.py
@@ -6,15 +6,21 @@ with open("requirements.txt") as f:
 
 setup(
     name="p01_codigosmed",  # Nombre del paquete
-    version="0.1.4",  # Versión inicial
+    version="0.1.5",  # Versión inicial
     author="Juan Paez",
-    author_email="jcpaez;@gmail.com",
-    description="Identificacion de los codigos IUM de medicamentos",
+    author_email="jcpaez@gmail.com",
+    description="Identificación de los códigos IUM de medicamentos",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/juancarlospaez/p01_codigosmed",  # URL del repositorio
     packages=find_packages(),  # Detecta automáticamente los paquetes
     install_requires=required,  # Usa las dependencias del archivo requirements.txt
+    
+    include_package_data=True,  # ✅ Permite incluir archivos no Python
+    package_data={
+        "ecum": ["../data/*.parquet"],  # ✅ Asegura que los archivos .parquet sean incluidos
+    },
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
