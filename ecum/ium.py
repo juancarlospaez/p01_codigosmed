8dcde88c157fbec272a3ca6b5195ffa7ee8fdfae
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-27 15:46:05 -0500

    Optimizacion de funciones

--- ecum/ium.py
+++ ecum/ium.py
@@ -60,8 +60,22 @@ def ClasTexARRAY(text):
     tokens = descripNorm(text)
     return np.array([(token, ClasToken(token)[1], i) for i, token in enumerate(tokens)], dtype=object)
 
+"""
 def ClasToken(token):
     return next(((token, k) for j, k in patrones if re.match(j, token)), (token, 'ER'))
+"""
+
+def ClasToken(token):
+    # Verificar que patrones exista y no sea None
+    if patrones is not None:
+        # Usar una lista por comprensión dentro del next()
+        return next(((token, k) for j, k in patrones if re.match(j, token)), (token, 'ER'))
+    else:
+        # Si patrones es None o está vacío, devolver un valor por defecto
+        return (token, 'ER')
+
+
+
 
 def descripNorm(text):
     return [word.upper() for word in wordpunct_tokenize(NormT1(text)) if word not in stopwords.words('spanish') and word != "."]
@@ -69,8 +83,19 @@ def descripNorm(text):
 def NormT1(string):
     return " ".join(re.sub(r"[^a-zA-Z0-9]", " ", unidecode.unidecode(string).lower()).split())
 
+"""
 def PATRONVAL(qw):
     return next((n for p, n in DICRE.get(qw[1], []) if re.match(p, qw[0])), 0)
+"""
+
+def PATRONVAL(qw):
+    # Verificar que DICRE no sea None
+    if DICRE is not None:
+        # Continuar con la búsqueda usando .get()
+        return next((n for p, n in DICRE.get(qw[1], []) if re.match(p, qw[0])), 0)
+    else:
+        # Si DICRE es None, devolver el valor por defecto
+        return 0
 """
 def packIUM(text):
     tokens = ClasTexARRAY(text)
@@ -82,6 +107,8 @@ def packIUM(text):
                             columns=['a', 'b', 'c']).drop_duplicates('a', keep='first')
     return [("".join(tokens[int(k1):int(k2), 0]), k3) for k1, k2, k3 in dfmatch.to_numpy()]
 """
+
+"""
 def packIUM(text):
     tokens = ClasTexARRAY(text)
     resultado1 = [PATRONVAL((token[0], token[1])) for token in tokens]
@@ -92,6 +119,28 @@ def packIUM(text):
                             columns=['a', 'b', 'c']).drop_duplicates('a', keep='first')
     unique_results = set((("".join(tokens[int(k1):int(k2), 0])), k3) for k1, k2, k3 in dfmatch.to_numpy())
     return list(unique_results)
+"""
+
+
+def packIUM(text):
+    tokens = ClasTexARRAY(text)
+    resultado1 = [PATRONVAL((token[0], token[1])) for token in tokens]
+    strVEC = "".join(["aaaa" if x == 0 else str(x) for x in resultado1])
+    
+    # Verificar si lpatrones existe y no es None
+    if lpatrones is not None:
+        dfmatch = pd.DataFrame([(int(match.start() / 4), int(match.end() / 4), jpatr[1])
+                                for jpatr in lpatrones for match in re.finditer(jpatr[0], strVEC)],
+                               columns=['a', 'b', 'c']).drop_duplicates('a', keep='first')
+        
+        # Verificar si dfmatch existe y tiene datos
+        if not dfmatch.empty:
+            # Crear conjunto de resultados únicos
+            unique_results = set((("".join(tokens[int(k1):int(k2), 0])), k3) for k1, k2, k3 in dfmatch.to_numpy())
+            return list(unique_results)
+    
+    # Si lpatrones es None o no hay coincidencias, devolver una lista vacía
+    return []
 
 """
 def packIUMuno(text):
@@ -113,10 +162,7 @@ def packIUMtotEXC(text):
     # Unimos los resultados con "|" o devolvemos el valor por defecto si no hay resultados
     return "|".join(unique_results) or "0,SIN_IUM"
 
-
-
 initialize()  # Se llama una sola vez para cargar los valores en memoria
-
 
 if __name__ == "__main__":
     textor = '1K1027361000103,IUM_Ok|1K1027361200102,IUM_Ok|AA1234561234123,Err_IUM_4|AA1234561234123,Err_IUM_4|AA1234561234123,Err_IUM_4|AA1234561234123,Err_IUM_4'
