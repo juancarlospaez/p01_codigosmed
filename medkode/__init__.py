08c9e55aade0309d3f00ec19636c1909eb12c88e
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-28 19:14:23 -0500

    Se elimina ium del espacio de nombres

--- medkode/__init__.py
+++ medkode/__init__.py
@@ -1,2 +1,7 @@
 from .ium import ium_pipe, ium_unique, ium_tuple
 __all__ = ["ium_pipe", "ium_unique", "ium_tuple"]
+
+# Eliminar el módulo 'ium' del espacio de nombres
+import sys
+if 'medkode.ium' in sys.modules:
+    del sys.modules['medkode.ium']
